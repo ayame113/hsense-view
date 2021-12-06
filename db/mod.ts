3a92423a358d180ac5c414a31623cee4b0d7a0e0
ayame113 <40050810+ayame113@users.noreply.github.com>
2021-12-07 07:08:48 +0900

    fix deploy error

--- db/mod.ts
+++ db/mod.ts
@@ -1,5 +1,5 @@
 import { FirebaseRealtimeDatabase } from "./realtime_db.ts";
-export { Writer } from "./realtime_db.ts";
+export type { Writer } from "./realtime_db.ts";
 
 export const database = new FirebaseRealtimeDatabase(
   JSON.parse(Deno.env.get("FIREBASE_CONFIG")!),
