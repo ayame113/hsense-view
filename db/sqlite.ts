3022805f53caa99100e45abeb126a613f776747e
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-01-16 07:23:16 +0900

    feat: add sqlite backend (#14)
    * fix ci
    
    * fix ci?
    
    * ???
    
    * tmp
    
    * tmp 2
    
    * remove dead code
    
    * working
    
    * ok
    
    * tmp
    
    * update
    
    * fix
    
    * feat: add sqlite backend
    
    * fix ci
    
    * fix ci 2

---
+++ db/sqlite.ts
@@ -0,0 +1,161 @@
+import { DB as Database } from "https://deno.land/x/sqlite@v3.2.0/mod.ts";
+import { ensureDir } from "https://deno.land/std@0.120.0/fs/mod.ts";
+import { delay } from "https://deno.land/std@0.120.0/async/mod.ts";
+
+import { hash } from "./utils.ts";
+import type {
+  Database as DatabaseInterface,
+  Writer as WriterInterface,
+} from "./types.ts";
+
+const DEFAULT_TIMEOUT = 120000;
+
+class SQLiteResources {
+  static #idToDB = new Map<string, Database>();
+  static #idToTimeout = new Map<string, number>();
+  static {
+    globalThis.addEventListener("unload", () => {
+      console.log("unload");
+      this.cleanUp();
+    });
+    globalThis.addEventListener("error", () => {
+      console.log("error");
+      this.cleanUp();
+    });
+  }
+  static async getDB(id: string) {
+    if (id.includes("/") || id.includes(".")) {
+      throw new Error("id cannot include . or /");
+    }
+    const db = this.#idToDB.get(id) ?? await this.#createDB(id);
+    clearTimeout(this.#idToTimeout.get(id));
+    this.#idToTimeout.set(
+      id,
+      setTimeout(() => this.#deleteDB(id), DEFAULT_TIMEOUT),
+    );
+    return db;
+  }
+  static async #createDB(id: string) {
+    await ensureDir("./graph_data");
+    const db = new Database(`./graph_data/${id}.db`, { mode: "create" });
+    this.#idToDB.set(id, db);
+    await delay(100);
+    console.log("createDB");
+    // テーブル1
+    // key: "id",val: ハッシュ値
+    db.query(`CREATE TABLE IF NOT EXISTS hash(
+      id TEXT PRIMARY KEY,
+      hash TEXT NOT NULL
+    )`);
+    // テーブル2
+    // key: 時刻(unix time),val: json値
+    db.query(`CREATE TABLE IF NOT EXISTS data(
+      time INTEGER PRIMARY KEY,
+      data TEXT NOT NULL
+    )`);
+    console.log("createDB end");
+    return db;
+  }
+  static #deleteDB(id: string) {
+    this.#idToDB.get(id)?.close();
+    this.#idToDB.delete(id);
+  }
+  static cleanUp() {
+    for (const [id, db] of this.#idToDB) {
+      console.log("delete", id);
+      db.close(true);
+    }
+  }
+}
+
+export class SQLiteDatabase implements DatabaseInterface {
+  async createToken(id: string): Promise<string | null> {
+    const token = crypto.randomUUID();
+    const hashedToken = await hash(token);
+    const db = await SQLiteResources.getDB(id);
+    let result: string | null = null;
+    db.transaction(() => {
+      const data = db.query("select hash from hash where id = 'id'");
+      if (data.length) {
+        return;
+      }
+      db.query("INSERT INTO hash VALUES ('id', ?)", [hashedToken]);
+      result = token;
+    });
+    return result;
+  }
+  async testToken(id: string, token: string) {
+    const db = await SQLiteResources.getDB(id);
+    const data = db.query("select hash from hash where id = 'id'");
+    const expectedHashedToken = await hash(token);
+    if (!data.length) {
+      return false;
+    }
+    const [[hashedToken]] = data;
+    return hashedToken === expectedHashedToken;
+  }
+  async getWriter(id: string, token: string) {
+    if (!await this.testToken(id, token)) {
+      return null;
+    }
+    return new Writer(id);
+  }
+  async getDataByLimit(
+    id: string,
+    { limit, fromTime }: {
+      limit?: number | undefined;
+      fromTime?: number | undefined;
+    } = {},
+  ): Promise<{ [key: string]: unknown; time: number }[]> {
+    const hasLimit = limit != null;
+    const hasFromTime = fromTime != null;
+    const db = await SQLiteResources.getDB(id);
+    if (hasLimit && hasFromTime) {
+      const data = db.query<string[]>(
+        "SELECT data FROM data WHERE time <= ? ORDER BY time DESC LIMIT ?",
+        [fromTime, limit],
+      );
+      return data.map(([r]) => JSON.parse(r));
+    } else if (hasLimit && !hasFromTime) {
+      const data = db.query<string[]>(
+        "SELECT data FROM data ORDER BY time DESC LIMIT ?",
+        [limit],
+      );
+      return data.map(([r]) => JSON.parse(r));
+    } else if (!hasLimit && hasFromTime) {
+      const data = db.query<string[]>(
+        "SELECT data FROM data WHERE time <= ? ORDER BY time DESC",
+        [fromTime],
+      );
+      return data.map(([r]) => JSON.parse(r));
+    } else if (!hasLimit && !hasFromTime) {
+      const data = db.query<string[]>(
+        "SELECT data FROM data ORDER BY time DESC",
+      );
+      return data.map(([r]) => JSON.parse(r));
+    } else {
+      throw new Error("unreachable");
+    }
+  }
+  // deno-lint-ignore require-await
+  async deleteDataByTime(_time: number) {
+    throw new Error("unimplemented");
+  }
+  cleanUp() {
+    SQLiteResources.cleanUp();
+  }
+}
+
+class Writer implements WriterInterface {
+  #id: string;
+  constructor(id: string) {
+    this.#id = id;
+  }
+  async write(data: { [key: string]: number; time: number }) {
+    const db = await SQLiteResources.getDB(this.#id);
+    db.query("INSERT INTO data VALUES (?, ?)", [
+      Math.round(data.time),
+      JSON.stringify(data),
+    ]);
+  }
+}
---
+++ db/sqlite_test.ts
@@ -0,0 +1,79 @@
+// deno-lint-ignore-file no-explicit-any
+
+import {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.117.0/testing/asserts.ts";
+import { deadline, delay } from "https://deno.land/std@0.117.0/async/mod.ts";
+import { SQLiteDatabase } from "./sqlite.ts";
+
+Deno.test("no error accessing window.location", () => {
+  console.log(window.location);
+});
+
+Deno.test({
+  name: "firebase realtime database",
+  fn: async () => {
+    return await deadline(
+      (async () => {
+        const id = `test-${Math.random()}`.replaceAll(".", "");
+        const db = new SQLiteDatabase();
+
+        const token = await db.createToken(id);
+        assert(token, "failed to get token");
+        assert(!await db.testToken(id, "wrong token was passed"));
+        assert(await db.testToken(id, token), "token is wrong");
+
+        await delay(2000); // 適切にwakeUpされることを確認する
+
+        const writer = await db.getWriter(id, token);
+        assert(writer, "writer is null");
+        for (let i = 0; i < 10; i++) {
+          await writer.write({ time: i, content: `i: ${i}` as any });
+        }
+        await delay(2000); // 適切にwakeUpされることを確認する
+        await writer.write({ time: 10, content: `i: 10` as any });
+
+        assertEquals(await db.getDataByLimit(id), [
+          { time: 10, content: "i: 10" },
+          { time: 9, content: "i: 9" },
+          { time: 8, content: "i: 8" },
+          { time: 7, content: "i: 7" },
+          { time: 6, content: "i: 6" },
+          { time: 5, content: "i: 5" },
+          { time: 4, content: "i: 4" },
+          { time: 3, content: "i: 3" },
+          { time: 2, content: "i: 2" },
+          { time: 1, content: "i: 1" },
+          { time: 0, content: "i: 0" },
+        ]);
+        assertEquals(await db.getDataByLimit(id, { fromTime: 4 }), [
+          { time: 4, content: "i: 4" },
+          { time: 3, content: "i: 3" },
+          { time: 2, content: "i: 2" },
+          { time: 1, content: "i: 1" },
+          { time: 0, content: "i: 0" },
+        ]);
+        assertEquals(await db.getDataByLimit(id, { limit: 5 }), [
+          { time: 10, content: "i: 10" },
+          { time: 9, content: "i: 9" },
+          { time: 8, content: "i: 8" },
+          { time: 7, content: "i: 7" },
+          { time: 6, content: "i: 6" },
+        ]);
+        assertEquals(await db.getDataByLimit(id, { fromTime: 3, limit: 2 }), [
+          { time: 3, content: "i: 3" },
+          { time: 2, content: "i: 2" },
+        ]);
+        await delay(5000);
+        db.cleanUp();
+        await delay(5000);
+        console.log("fin");
+      })(),
+      30 * 1000,
+    );
+  },
+  // https://github.com/firebase/firebase-js-sdk/issues/5783
+  sanitizeOps: false,
+  sanitizeResources: false,
+});
