4aa162de41c3902f92759c9fb646b26377af8fe3
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-01-23 08:28:28 +0900

    fmt test

--- dev/typecheck.ts
+++ dev/typecheck.ts
@@ -1,3 +1,10 @@
 // deno-lint-ignore-file
-import type {} from "https://deno.land/x/deploy_type_checker@v0.0.4/dts/deno.window.d.ts";
+import type {
+
+
+
+
+
+
+} from "https://deno.land/x/deploy_type_checker@v0.0.4/dts/deno.window.d.ts";
 import "../serve.ts";
