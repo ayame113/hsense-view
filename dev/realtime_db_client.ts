171e1a47411c034d30592588511864f9fa1340bd
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-02-02 10:45:39 +0900

    add dev code

---
+++ dev/realtime_db_client.ts
@@ -0,0 +1,27 @@
+// 計測用コード
+
+import "https://deno.land/x/dotenv@v3.1.0/load.ts";
+
+// @deno-types="https://cdn.esm.sh/v58/firebase@9.6.0/app/dist/app/index.d.ts"
+import {
+  deleteApp,
+  FirebaseOptions,
+  initializeApp,
+} from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
+// @deno-types="https://cdn.esm.sh/v58/firebase@9.6.0/database/dist/database/index.d.ts"
+import {
+  getDatabase,
+  onValue,
+  ref,
+  set,
+} from "https://www.gstatic.com/firebasejs/9.6.0/firebase-database.js";
+
+const config = JSON.parse(Deno.env.get("FIREBASE_CONFIG_TEST")!);
+const app = initializeApp(config);
+const db = getDatabase(app);
+
+const dbRef = ref(db);
+onValue(dbRef, (snapshot) => {
+  const data = snapshot.val();
+  console.log(Date.now() - data);
+});
