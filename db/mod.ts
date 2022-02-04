517386f86b92fd33effe5cae5a37ee3bd326c773
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-02-04 17:22:03 +0900

    remove dynamic import (#30)
    * remove dynamic import
    
    * comment out

--- db/mod.ts
+++ db/mod.ts
@@ -1,8 +1,15 @@
 import { config } from "https://deno.land/x/dotenv@v3.1.0/mod.ts";
 import { FirebaseRealtimeDatabase } from "./realtime_db.ts";
+// import { SQLiteDatabase } from "./sqlite3.ts";
 // import { SQLiteDatabase } from "./sqlite.ts";
 import { Database, Writer } from "./types.ts";
 import { isDeploy } from "./utils.ts";
+
+if (!isDeploy()) {
+  throw new Error("https://github.com/ayame113/socket-graph/pull/30", {
+    cause: "https://github.com/denoland/deploy_feedback/issues/166",
+  });
+}
 
 let database: Database;
 try {
@@ -13,8 +20,12 @@ try {
   } else {
     // configAscyncじゃなくて同期バージョンを使う必要がある（deploy不可）
     config({ export: true });
-    const { SQLiteDatabase } = await import("./sqlite3.ts");
-    database = new SQLiteDatabase();
+    // 動的importが使えない
+    // const { SQLiteDatabase } = await import("./sqlite3.ts");
+    throw new Error("https://github.com/ayame113/socket-graph/pull/30", {
+      cause: "https://github.com/denoland/deploy_feedback/issues/166",
+    });
+    // database = new SQLiteDatabase();
   }
 } catch (error) {
   console.warn(error);
