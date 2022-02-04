a442863339cee76d843187fb258c36ab6b921cff
GitHub <noreply@github.com>
2022-02-04 08:22:22 +0000

    chore(deps): update deno dependencies

--- listeners/ws/db_test.ts
+++ listeners/ws/db_test.ts
@@ -1,6 +1,6 @@
 // deno-lint-ignore-file no-explicit-any
-import { assertEquals } from "https://deno.land/std@0.123.0/testing/asserts.ts";
-import { deferred, delay } from "https://deno.land/std@0.123.0/async/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.125.0/testing/asserts.ts";
+import { deferred, delay } from "https://deno.land/std@0.125.0/async/mod.ts";
 
 import { database } from "../../db/mod.ts";
 import { mockFn, MockWebSocket } from "../../static/utils/test_mock.ts";
