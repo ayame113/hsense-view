97066774317278612db85fbf158240ed9ad0a48f
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-02-08 18:08:59 +0900

    fix 表示

--- dev/dummy_meter.ts
+++ dev/dummy_meter.ts
@@ -3,16 +3,16 @@ import { delay } from "https://deno.land/std@0.125.0/async/mod.ts";
 // TODO:
 // closeしたときに再接続
 
-// const api = "wss://socket-graph.deno.dev";
-const api = "ws://localhost:8000";
-// const TOKEN = "d09d3670-6e08-45cf-935d-cf6cc30a3d5a"; // 本番環境用
-const TOKEN = "c9151f9e-2dbc-4ad8-977d-0a95dd641a9f"; // localhost用
+const api = "wss://socket-graph.deno.dev";
+// const api = "ws://localhost:8000";
+const TOKEN = "d09d3670-6e08-45cf-935d-cf6cc30a3d5a"; // 本番環境用
+// const TOKEN = "c9151f9e-2dbc-4ad8-977d-0a95dd641a9f"; // localhost用
 
 // websocketで送信するやつ
 const id = "水道メーター1";
 const socktURL = `${api}/ws/send/${id}`;
 
-let 流量 = 0;
+let 積算流量 = 0;
 
 while (true) {
   // 無限に再接続処理
@@ -30,13 +30,13 @@ while (true) {
     while (true) {
       await delay(wait);
       const time = Date.now();
-      const 流速origin = 7000 < time % 14000 ? 0.9 : 0.1;
-      const 流速 = 流速origin + Math.random() * 0.1;
-      流量 += 流速 * (wait / 1000 / 3600); // 1秒は(1/3600)時間
+      const 瞬時流量origin = 7000 < time % 14000 ? 0.9 : 0.1;
+      const 瞬時流量 = 瞬時流量origin + Math.random() * 0.1;
+      積算流量 += 瞬時流量 * (wait / 1000 / 3600); // 1秒は(1/3600)時間
       const data = {
         time,
-        "流速（m^3）": 流量,
-        "流量（m^3／h）": 流速,
+        "積算流量（m^3）": 積算流量,
+        "瞬時流量（m^3／h）": 瞬時流量,
       };
       console.log(data);
       socket.send(JSON.stringify(data));
