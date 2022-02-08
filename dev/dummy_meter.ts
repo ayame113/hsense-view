006819740bcdaba5dc3a7d042340cac6ffed9ee7
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-02-08 13:33:35 +0900

    tmp

--- dev/dummy_meter.ts
+++ dev/dummy_meter.ts
@@ -1,26 +1,46 @@
 import { delay } from "https://deno.land/std@0.125.0/async/mod.ts";
 
+// TODO:
+// closeしたときに再接続
+
 // const api = "ws://localhost:8000";
 const api = "wss://socket-graph.deno.dev";
 
 // websocketで送信するやつ
 const id = "水道メーター1";
 const TOKEN = "d09d3670-6e08-45cf-935d-cf6cc30a3d5a";
-const socket = new WebSocket(`${api}/ws/send/${id}`);
-socket.addEventListener("close", console.log);
+const socktURL = `${api}/ws/send/${id}`;
 
-await new Promise((ok) => socket.addEventListener("open", ok));
+let 流量 = 0;
 
-socket.send(TOKEN);
-
-const wait = 2000;
 while (true) {
-  await delay(wait);
-  const time = Date.now();
-  const data = {
-    time,
-    "流速（m^3／h）": 0.5 + Math.random() * 0.1,
-  };
-  console.log(data);
-  socket.send(JSON.stringify(data));
+  // 無限に再接続処理
+  try {
+    const socket = new WebSocket(socktURL);
+
+    await new Promise((resolve, reject) => {
+      socket.addEventListener("open", resolve);
+      socket.addEventListener("close", reject);
+    });
+
+    socket.send(TOKEN);
+
+    const wait = 2000;
+    while (true) {
+      await delay(wait);
+      const time = Date.now();
+      const 流速 = 0.5 + Math.random() * 0.1;
+      流量 += 流速;
+      const data = {
+        time,
+        "流速（m^3）": 流速,
+        "流量（m^3／h）": 流量,
+      };
+      console.log(data);
+      socket.send(JSON.stringify(data));
+    }
+  } catch (error) {
+    console.log(error);
+    await delay(2000);
+  }
 }
