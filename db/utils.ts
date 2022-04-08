5be0d72214cc8d1ae07ba69fb6ecd4bbd723c62a
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-04-08 17:31:54 +0900

    chore: update readme (#48)
    * fix firebase security rule
    
    * chore: fix test
    
    * fix tests
    
    * update

--- db/utils.ts
+++ db/utils.ts
@@ -4,9 +4,14 @@ const encoder = new TextEncoder();
 const decoder = new TextDecoder();
 /** SHA-256を使用して文字列をハッシュ化します。 */
 export async function hash(message: string) {
+  // 文字列をUint8Arrayに変換
   const encoded = encoder.encode(message);
+  // SHA-256でハッシュ化
   const hashBuffer = await crypto.subtle.digest("SHA-256", encoded);
-  return decoder.decode(hex.encode(new Uint8Array(hashBuffer)));
+  // 16進数文字列のUint8Arrayに変換
+  const hashed = hex.encode(new Uint8Array(hashBuffer));
+  // Uint8Arrayを文字列に変換
+  return decoder.decode(hashed);
 }
 
 /** deno deployで実行されているかどうかを判定 */
